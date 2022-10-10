44ead7785a8754a39c0802344a05b333ca3a6e14
Your NameProger <ssssu@mail.com>
2022-10-10 23:56:13 +0800

    Добавьте файлы проекта.

---
+++ WinFormsApp3/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿
+namespace WinFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.TxtFirst = new System.Windows.Forms.TextBox();
+            this.TxtSecond = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(130, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 75);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TxtFirst
+            // 
+            this.TxtFirst.Location = new System.Drawing.Point(12, 12);
+            this.TxtFirst.Name = "TxtFirst";
+            this.TxtFirst.Size = new System.Drawing.Size(84, 23);
+            this.TxtFirst.TabIndex = 1;
+            // 
+            // TxtSecond
+            // 
+            this.TxtSecond.Location = new System.Drawing.Point(12, 58);
+            this.TxtSecond.Name = "TxtSecond";
+            this.TxtSecond.Size = new System.Drawing.Size(84, 23);
+            this.TxtSecond.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 113);
+            this.Controls.Add(this.TxtSecond);
+            this.Controls.Add(this.TxtFirst);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox TxtFirst;
+        private System.Windows.Forms.TextBox TxtSecond;
+    }
+}
+
---
+++ WinFormsApp3/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Properties.Settings.Default.FirstWord1 = TxtFirst.Text;
+            Properties.Settings.Default.SecondWord2 = TxtSecond.Text;
+
+            MessageBox.Show(Logic.Compare(TxtFirst.Text, TxtSecond.Text));
+        }
+    }
+
+    public class Logic
+    {
+        public static string Compare(string FirstString, string SecondString)
+        {
+            string outMessage = string.Empty;
+
+            for (int i = 0; i <= FirstString.Length - 2; i++)
+            {
+                for (int j = i + 1; j <= FirstString.Length - 1; j++)
+                {
+                    if (FirstString[i] == FirstString[j])
+                        FirstString = FirstString.Remove(j, 1);
+                }
+            }
+
+
+            for (int i = 0; i < FirstString.Length; i++)
+            {
+                if (SecondString.Contains(FirstString[i]))
+                    outMessage += "Да ";
+
+                else
+                    outMessage += "Нет ";
+            }
+
+            return outMessage;
+        }
+
+    }
+}
